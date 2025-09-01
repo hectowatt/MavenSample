426cfb287d27a690366a88346587149f0704d37b
hectowatt <w477.dev@gmail.com>
2025-09-01 14:47:02 +0900

    クライアント側でXMLのパースができるよう修正

--- src/main/java/mavensample/client/ApiClient.java
+++ src/main/java/mavensample/client/ApiClient.java
@@ -1,16 +1,35 @@
 package mavensample.client;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import mavensample.model.Greeting;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 
-import org.glassfish.jersey.client.JerseyClientBuilder;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
 
 public class ApiClient{
     public static void main(String[] args){
-        Client client = JerseyClientBuilder.createClient();
-        WebTarget target = client.target("http://localhost:8080/");
+        try {
+            Client client = ClientBuilder.newClient();
+            Response response = client.target("http://localhost:8080/api/hello").request().get();
 
-        String response = target.request().get(String.class);
-        System.out.println("Response from Mock API Server: " + response);
+            String xml = response.readEntity(String.class);
+            System.out.println("XML:" + xml);
+
+            // Jacksonでパースする
+            XmlMapper xmlmapper = new XmlMapper();
+            Greeting greeting = xmlmapper.readValue(xml, Greeting.class);
+            System.out.println("パース後のオブジェクト:" + greeting);
+            System.out.println("Message:" + greeting.getMessage());
+            System.out.println("From:" + greeting.getFrom());
+
+            response.close();
+            client.close();
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("エラー発生");
+        }
     }
 }
